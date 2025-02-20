59126fbbaebf792371b3949ac81e8bba6c0f52a3
AndreiDanaila <danaila95andrei@gmail.com>
2025-02-20 03:53:43 +0200

    worked on delete part

--- admin/delete-admin.php
+++ admin/delete-admin.php
@@ -1,5 +1,9 @@
 <?php
 
+    // 1. Get the id of Admin to be deleted
+    echo $id = $_GET['id']; 
+    // 2. Create SQL Query to Delete Admin
 
+    // 3. Redirect to Manage Admin Page with message (success/error)
 
 ?>
